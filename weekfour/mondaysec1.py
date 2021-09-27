635f502e41494bd8b7b286e8238dd457f6fe9b07
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-09-27 12:24:14 -0700

    Create mondaysec1.py

---
+++ weekfour/mondaysec1.py
@@ -0,0 +1,18 @@
+
+def delete_HTML(text):
+    '''
+    This function removes all HTML tags from the input text.
+    >>> delete_HTML('This is <b>bold</b>!')
+    'This is bold!'
+    >>> delete_HTML('This is <i>italic</i>!')
+    'This is italic!'
+    >>> delete_HTML('This is <strong>italic</strong> and this is <ins>strikethrough</ins>!')
+    'This is italic and this is strikethrough!'
+    '''
+
+    # acculumator
+    new_text = ''
+    for i in range(len(text)):
+        # do something with text[i]
+        new_text += text[i]
+    return new_text
