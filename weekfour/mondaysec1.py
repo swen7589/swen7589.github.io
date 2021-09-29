55cb18798434573edc13a111df45fc744268f7f3
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-09-29 12:38:23 -0700

    Update mondaysec1.py

--- weekfour/mondaysec1.py
+++ weekfour/mondaysec1.py
@@ -10,9 +10,42 @@ def delete_HTML(text):
     'This is italic and this is strikethrough!'
     '''
 
-    # acculumator
+    '''
+    text = text.replace('<b>', '')
+    text = text.replace('</b>', '')
+    text = text.replace('<i>', '')
+    text = text.replace('</i>', '')
+    '''
+
+    # .replace is good for an exact string 
+    # but not good for patterns
+
+    '''
     new_text = ''
+    inside_tag = False # will be True inside < >, and False otherwise 
+    
     for i in range(len(text)):
-        # do something with text[i]
-        new_text += text[i]
+
+        if text[i] == '<':
+            inside_tag = True
+
+        elif text[i] == '>':
+            inside_tag = False
+
+        elif inside_tag == False:
+            new_text += text[i]
+
+    return new_text
+    '''
+
+    new_text = ''
+
+    lessthans = text.split('<')
+
+    for lessthan in lessthans:
+        greaterthans = lessthan.split('>')
+        # for greaterthan in greaterthans:
+            # print('greaterthan =', greaterthan)
+        new_text += greaterthans[-1]
+        # print('lessthan =', lessthan)
     return new_text
