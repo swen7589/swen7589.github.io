d5ca03f77525323d7de04835a3302b872e44c6a8
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-10-11 11:43:31 -0700

    Update classwork.py

--- weeksix/classwork.py
+++ weeksix/classwork.py
@@ -2,3 +2,15 @@ print('hello world')
 
 # absolute path - path that works anywhere, references the file no matter where you're located
 # /Users/swenihueze/Documents/GitHub/swen7589.github.io/weeksix/classwork.py
+
+# the computer always converts relative paths to absolute to access a file
+
+# pwd - print working directory
+# working directory is where you are located
+# directory is a folder
+
+# put the working directory at the beginning of the relative path to make the absolute path
+
+# cd - change directory
+
+# 
