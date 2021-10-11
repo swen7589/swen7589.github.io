8d5cf1c72335e8dbeb18c8b91d8ab194a7ede0e4
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-10-11 11:36:55 -0700

    Update classwork.py

---
+++ weeksix/classwork.py
@@ -0,0 +1,4 @@
+print('hello world')
+
+# absolute path - path that works anywhere, references the file no matter where you're located
+# /Users/swenihueze/Documents/GitHub/swen7589.github.io/weeksix/classwork.py
