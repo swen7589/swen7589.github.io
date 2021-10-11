8a3f69d62d95d580b4a638671ca37fabb6a752d0
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-10-11 12:25:11 -0700

    Update classwork.py

--- weeksix/classwork.py
+++ weeksix/classwork.py
@@ -13,4 +13,31 @@ print('hello world')
 
 # cd - change directory
 
-# 
+# cd .. - go out of the folder 
+
+# with open('path_to_file') as f:
+#   text = f.read()
+# print(text)
+
+# if you make a file, insert name of file into ' ' (where path_to_file is)
+# the .read() function reads the file (the characters, not just string) and prints it out
+
+# folder/ - to get to file in different folder, put that into ' ' (before path_to_file)
+
+# import os         # os = operating system
+
+# os.getcwd()       # cwd = current working directory
+# returns string of working directory
+
+# Rule 1: know your working directory
+# Rule 2: know your file encoding
+
+# encoding = utf-8 (default on mac)
+
+# put the encoding after ' ' in with open()
+
+# if you use the open function, you must specify the encoding
+
+
+
+
