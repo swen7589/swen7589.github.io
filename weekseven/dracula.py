1c22029c2aad0fe874d97c613f4ccc66321b4665
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-11-02 19:14:31 -0700

    lab halloweekend

---
+++ weekseven/dracula.py
@@ -0,0 +1,65 @@
+
+# step 2 part 1
+with open('dracula.html', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# step 2 part 1
+
+text = text.replace('Count', 'Professor')
+text = text.replace('count', 'professor')
+
+text = text.replace('Dracula', 'Izbicki')
+text = text.replace('dracula', 'izbicki')
+text = text.replace('DRACULA', 'IZBICKI')
+text = text.replace('D R A C U L A', 'I Z B I C K I')
+
+text = text.replace('Bram', 'Swen')
+text = text.replace('Stoker', 'Ihueze')
+
+with open('izbicki.html', 'w', encoding='utf-8') as f:
+    f.write(text)
+
+
+'''
+accumulator = []
+
+for i in dracula.html:
+
+    # for count turning into professor
+    if i == 'Count':
+        i = 'Professor'
+        accumulator.append(i)
+    elif i == 'count':
+        i = 'professor'
+        accumulator.append(i)
+    
+    # for dracula turning into izbicki
+    elif i == 'Dracula':
+        i = 'Izbicki'
+        accumulator.append(i)
+    elif i == 'dracula':
+        i = 'izbicki'
+        accumulator.append(i)
+    elif i == 'DRACULA':
+        i = 'IZBICKI'
+        accumulator.append(i)
+    
+    # for the edge case
+    elif i == 'D R A C U L A':
+        i = 'I Z B I C K I'
+        accumulator.append(i)
+
+    # for my name
+    elif i == 'Bram':
+        i = 'Swen'
+        accumulator.append(i)
+    elif i == 'Stroker'
+        i = 'Ihueze'
+        accumulator.append(i)
+    
+    # any other word
+    else:
+        accumulator.append(i)
+
+return accumulator.join('')
+'''
