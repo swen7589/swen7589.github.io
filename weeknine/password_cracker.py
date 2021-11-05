84b4f48ae8b61aa30c6ddb9ed1ac1d348c2babac
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-11-05 12:31:10 -0700

    Update password_cracker.py

--- weeknine/password_cracker.py
+++ weeknine/password_cracker.py
@@ -1,19 +1,21 @@
 
 from zipfile import ZipFile
 
-# step 3 part 1
 with open('Ashley-Madison.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 passwords = [line.strip() for line in f]
 
 for password in passwords:
-    if password == True:
+    if password:
         f.extractall(pwd=password)
         print(password)
     try:
         with ZipFile('whitehouse_secrets.zip') as zf:
-            text = f.read()
+            text = zf.read()
+    except RuntimeError:
+        pass
+
 
 
 
